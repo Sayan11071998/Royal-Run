7802678c8dafc6ddff6abb4658286c30fbc533ed
Sayan Nandi <sayannandi11071998@gmail.com>
2025-04-23 13:06:25 +0530

    Skybox and Movement clamp added

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,8 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float xClamp = 3f;
+    [SerializeField] private float zClamp = 3f;
 
     private Vector2 movement;
     private Rigidbody rigidBody;
@@ -28,6 +30,9 @@ public class PlayerController : MonoBehaviour
         Vector3 currentPosition = rigidBody.position;
         Vector3 moveDirection = new Vector3(movement.x, 0, movement.y);
         Vector3 newPosition = currentPosition + moveDirection * (moveSpeed * Time.fixedDeltaTime);
+
+        newPosition.x = Mathf.Clamp(newPosition.x, -xClamp, xClamp);
+        newPosition.z = Mathf.Clamp(newPosition.z, -zClamp, zClamp);
 
         rigidBody.MovePosition(newPosition);
     }
