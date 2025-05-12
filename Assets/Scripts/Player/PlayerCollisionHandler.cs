4e8de0a05c3c33d33e91a5a3a2a75224e3f41dc1
Sayan Nandi <sayannandi11071998@gmail.com>
2025-05-12 09:35:10 +0530

    Player Hit implemented

--- Assets/Scripts/Player/PlayerCollisionHandler.cs
+++ Assets/Scripts/Player/PlayerCollisionHandler.cs
@@ -2,8 +2,23 @@ using UnityEngine;
 
 public class PlayerCollisionHandler : MonoBehaviour
 {
+    [SerializeField] private Animator animator;
+    [SerializeField] private float collisionCooldown = 1f;
+
+    private const string hitString = "Hit";
+
+    private float cooldownTimer = 0f;
+
+    private void Update()
+    {
+        cooldownTimer += Time.deltaTime;
+    }
+
     private void OnCollisionEnter(Collision other)
     {
-        Debug.Log(other.gameObject.name);
+        if (cooldownTimer < collisionCooldown) return;
+
+        animator.SetTrigger(hitString);
+        cooldownTimer = 0f;
     }
 }
